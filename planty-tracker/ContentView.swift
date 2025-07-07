1916b85a7f0dbb7cbe95d5d31596f2b7cadafa18
dariakozlovska <odriy.lovska@gmail.com>
2025-07-07 02:48:12 +0200

    Initial Commit

---
+++ planty-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  planty-tracker
+//
+//  Created by Nikita Pidhornyi on 07/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ planty-tracker/planty_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  planty_trackerApp.swift
+//  planty-tracker
+//
+//  Created by Nikita Pidhornyi on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct planty_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
