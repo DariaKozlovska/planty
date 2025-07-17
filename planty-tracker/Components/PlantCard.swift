c21ee746fdcb1a23bc4520a97297e851106366b3
dariakozlovska <odriy.lovska@gmail.com>
2025-07-18 00:13:48 +0200

    Fixning bugs in progress bar part 1

--- planty-tracker/Components/PlantCard.swift
+++ planty-tracker/Components/PlantCard.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct PlantCard: View {
+    
+    @EnvironmentObject var themeManager: ThemeManager
     let plant: Plant
     
     var body: some View {
@@ -17,17 +19,16 @@ struct PlantCard: View {
                     Image(uiImage: image)
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 50, height: 50)
                         .clipShape(Circle())
+                        .frame(width: 100, height: 100)
                         .shadow(radius: 4)
                 } else {
-                    Image(systemName: "leaf.fill")
+                    Image("Plant-2")
                         .resizable()
                         .scaledToFit()
-                        .foregroundColor(.green)
                         .frame(width: 50, height: 50)
                         .padding(12)
-                        .background(Color.green.opacity(0.15))
+                        .background(Color.darkTeal.opacity(0.15))
                         .clipShape(Circle())
                 }
             }
@@ -36,7 +37,7 @@ struct PlantCard: View {
             VStack(alignment: .leading){
                 Text(plant.name)
                     .font(.custom("Exo2-SemiBold", size: 18))
-                    .foregroundColor(.primary)
+                    .foregroundColor(.blackTeal)
                     .lineLimit(1)
                     .minimumScaleFactor(0.75)
                 if let last = plant.lastWateredDates.sorted().last {
@@ -50,9 +51,10 @@ struct PlantCard: View {
         .frame(maxWidth: .infinity)
         .background(
             RoundedRectangle(cornerRadius: 20)
-                .fill(Color(UIColor.secondarySystemBackground))
+//                .fill(Color(UIColor.secondarySystemBackground))
+                .fill(Color.white)
                 .shadow(color: Color.black.opacity(0.1), radius: 6, x: 0, y: 3)
         )
-        .padding(.horizontal, 8)
+//        .padding(.horizontal, 8)
     }
 }
--- planty-tracker/Components/WaterProgressBar.swift
+++ planty-tracker/Components/WaterProgressBar.swift
@@ -7,14 +7,16 @@
 
 import SwiftUI
 
-struct WaterProgressBar: View{
+struct WaterProgressBar: View {
     let lastWatered: Date
     let frequency: Int
     
-    var body: some View{
+    @State private var now = Date()
+    
+    var body: some View {
         let totalDays = frequency
-        let daysSince = Calendar.current.dateComponents([.day], from: lastWatered, to: Date()).day ?? 0
-        let progress = min(Double(daysSince) / Double(totalDays), 1.0)
+        let daysSince = Calendar.current.dateComponents([.second], from: lastWatered, to: now).day ?? 0
+        let progress = min(Double(daysSince) / 10.0, 1.0)
         let daysRemaining = max(totalDays - daysSince, 0)
         
         VStack(alignment: .leading, spacing: 8){
@@ -37,7 +39,7 @@ struct WaterProgressBar: View{
                         .frame(height: 6)
 
                     Capsule()
-                        .fill(daysRemaining > 0 ? Color.green : Color.red)
+                        .fill(daysRemaining > 0 ? Color.darkTeal : Color.red)
                         .frame(width: geometry.size.width * CGFloat(progress), height: 6)
                         .animation(.easeInOut(duration: 0.5), value: progress)
                 }
@@ -45,6 +47,13 @@ struct WaterProgressBar: View{
             .frame(height: 6)
         }
         .padding(.vertical, 8)
+        .onReceive(
+            Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+        ) { newDate in
+            print("⏰ Timer triggered at: \(newDate)")
+            print(Date())
+            self.now = newDate
+        }
     }
     
     private func formattedDate(_ date: Date) -> String {
